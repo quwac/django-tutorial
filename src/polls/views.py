5b9bb27348159821c1ecf397dc50ba17a4fb8fa5
quwac <53551867+quwac@users.noreply.github.com>
2020-12-28 00:46:52 +0900

    Add tutorial 4 before Amend URLconf

--- src/polls/views.py
+++ src/polls/views.py
@@ -1,7 +1,8 @@
-from django.http import HttpRequest, HttpResponse
+from django.http import HttpRequest, HttpResponse, HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render
+from django.urls import reverse
 
-from polls.models import Question
+from polls.models import Choice, Question
 
 
 def index(request: HttpRequest) -> HttpResponse:
@@ -15,9 +16,21 @@ def detail(request: HttpRequest, question_id: int) -> HttpResponse:
 
 
 def results(request: HttpRequest, question_id: int) -> HttpResponse:
-    response = "You're looking at the results of question %s."
-    return HttpResponse(response % question_id)
+    question: Question = get_object_or_404(Question, pk=question_id)
+    return render(request, "polls/results.html", {"question": question})
 
 
 def vote(request: HttpRequest, question_id: int) -> HttpResponse:
-    return HttpResponse("You're voting on questions %s." % question_id)  # noqa: FS001
+    question: Question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST["choice"])  # type: ignore
+    except (KeyError, Choice.DoesNotExist):
+        return render(
+            request,
+            "polls/detail.html",
+            {"question": question, "error_message": "You did not select a choice."},
+        )
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        return HttpResponseRedirect(reverse("polls:results", args=(question.id,)))  # type: ignore
