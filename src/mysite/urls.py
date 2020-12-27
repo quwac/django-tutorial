eb0780236516248040e76a0a219d24744447e042
quwac <53551867+quwac@users.noreply.github.com>
2020-12-27 16:18:11 +0900

    Add tutorial 2

--- src/mysite/urls.py
+++ src/mysite/urls.py
@@ -18,6 +18,6 @@ from django.urls import path
 from django.urls.conf import include
 
 urlpatterns = [
-    path('polls/', include('polls.urls')),
-    path('admin/', admin.site.urls),
+    path("polls/", include("polls.urls")),
+    path("admin/", admin.site.urls),
 ]
--- src/polls/models.py
+++ src/polls/models.py
@@ -1,3 +1,27 @@
-from django.db import models
+import datetime
 
-# Create your models here.
+from django.db import models
+from django.utils import timezone
+
+
+class Question(models.Model):
+    question_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField("date published")
+
+    def was_published_recentry(self) -> bool:
+        sub = timezone.now() - datetime.timedelta(days=1)
+        return self.pub_date >= sub  # pyright:  reportUnknownVariableType = false
+
+    def __str__(self) -> str:
+        return self.question_text
+
+
+class Choice(models.Model):
+    question = models.ForeignKey(
+        Question, on_delete=models.CASCADE
+    )  # pyright: reportUnknownArgumentType = false
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
+
+    def __str__(self) -> str:
+        return self.choice_text
