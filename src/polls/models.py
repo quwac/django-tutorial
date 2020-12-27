e9132a3c6d15ceac8e14147e15afe8ca1650579a
quwac <53551867+quwac@users.noreply.github.com>
2020-12-28 02:01:28 +0900

    Add tutorial 5

--- src/polls/models.py
+++ src/polls/models.py
@@ -1,3 +1,5 @@
+# pyright: reportUnknownVariableType=false
+# pyright: reportUnknownArgumentType=false
 import datetime
 
 from django.db import models
@@ -9,17 +11,15 @@ class Question(models.Model):
     pub_date = models.DateTimeField("date published")
 
     def was_published_recentry(self) -> bool:
-        sub = timezone.now() - datetime.timedelta(days=1)
-        return self.pub_date >= sub  # pyright:  reportUnknownVariableType = false
+        now = timezone.now()
+        return now - datetime.timedelta(days=1) <= self.pub_date <= now
 
     def __str__(self) -> str:
         return self.question_text
 
 
 class Choice(models.Model):
-    question = models.ForeignKey(
-        Question, on_delete=models.CASCADE
-    )  # pyright: reportUnknownArgumentType = false
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
 
--- src/polls/tests.py
+++ src/polls/tests.py
@@ -1,3 +1,86 @@
-# from django.test import TestCase
+# pyright: reportGeneralTypeIssues = false
+# pyright: reportUnknownArgumentType = false
+from datetime import timedelta
 
-# Create your tests here.
+from django.test import TestCase
+from django.urls import reverse
+from django.utils import timezone
+
+from polls.models import Question
+
+
+def create_question(question_text: str, days: int) -> Question:
+    time = timezone.now() + timedelta(days=days)
+    return Question.objects.create(question_text=question_text, pub_date=time)
+
+
+class QuestionDetailViewTests(TestCase):
+    def test_future_question(self):
+        future_question = create_question("Future question.", 5)
+        url = reverse("polls:detail", args=(future_question.id,))
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 404)
+
+    def test_past_question(self):
+        past_question = create_question("Past question.", -5)
+        url = reverse("polls:detail", args=(past_question,))
+        response = self.client.get(url)
+        self.assertContains(response, past_question.question_text)
+
+
+class QuestionIndexViewTests(TestCase):
+    def test_no_questions(self):
+        response = self.client.get(reverse("polls:index"))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "No polls are available.")
+        self.assertQuerysetEqual(response.context["latest_question_list"], [])
+
+    def test_past_question(self):
+        _ = create_question("Past question.", -30)
+        response = self.client.get(reverse("polls:index"))
+        self.assertQuerysetEqual(
+            response.context["latest_question_list"], ["<Question: Past question.>"]
+        )
+
+    def test_future_question(self):
+        _ = create_question("Future question.", 30)
+        response = self.client.get(reverse("polls:index"))
+        self.assertContains(response, "No polls are available.")
+        self.assertQuerysetEqual(response.context["latest_question_list"], [])
+
+    def test_future_question_and_past_question(self):
+        _ = create_question("Past question.", -30)
+        _ = create_question("Future question.", 30)
+        response = self.client.get(reverse("polls:index"))
+        self.assertQuerysetEqual(
+            response.context["latest_question_list"], ["<Question: Past question.>"]
+        )
+
+    def test_two_past_questions(self):
+        _ = create_question("Past question 1.", -30)
+        _ = create_question("Past question 2.", -5)
+        response = self.client.get(reverse("polls:index"))
+        self.assertQuerysetEqual(
+            response.context["latest_question_list"],
+            [
+                "<Question: Past question 2.>",
+                "<Question: Past question 1.>",
+            ],
+        )
+
+
+class QuestionModelTests(TestCase):
+    def test_was_published_recently_with_future_question(self):
+        time = timezone.now() + timedelta(days=30)
+        future_question = Question(pub_date=time)
+        self.assertIs(future_question.was_published_recentry(), False)
+
+    def test_was_published_recently_with_old_question(self):
+        time = timezone.now() - timedelta(days=1, seconds=1)
+        old_question = Question(pub_date=time)
+        self.assertIs(old_question.was_published_recentry(), False)
+
+    def test_was_published_recently_with_recent_question(self):
+        time = timezone.now() - timedelta(hours=23, minutes=59, seconds=59)
+        recent_question = Question(pub_date=time)
+        self.assertIs(recent_question.was_published_recentry(), True)
